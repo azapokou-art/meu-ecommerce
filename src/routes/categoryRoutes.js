4e3193511552ca02891f4928847e9d9d91c2544d
azapokou-art <azapokou@gmail.com>
2025-10-28 00:29:36 -0300

    feat: implementar sistema de categorias para produtos

---
+++ src/routes/categoryRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const categoryController = require('../controllers/categoryController');
+const authMiddleware = require('../middlewares/authMiddleware');
+
+const router = express.Router();
+
+
+router.get('/', categoryController.getAll);
+router.get('/:id', categoryController.getById);
+router.get('/:id/products', categoryController.getProducts);
+
+
+router.post('/', authMiddleware, categoryController.create);
+
+module.exports = router;
