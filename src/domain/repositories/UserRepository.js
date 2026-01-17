f9dc8201c805d57cd23b809e8acb1e26e8e19ca7
azapokou-art <azapokou@gmail.com>
2026-01-17 02:42:08 -0300

    Adiciona findById(id no UserRepository

--- src/domain/repositories/UserRepository.js
+++ src/domain/repositories/UserRepository.js
@@ -11,5 +11,8 @@ class UserRepository {
     async save(user) {
         throw new Error('Method not implemented');
     }
+    async findById(id) {
+        throw new Error('Method not implemented');
+    }
 }
 module.exports = UserRepository;
