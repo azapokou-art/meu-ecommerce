8fd862cced897d94a63f0332928a8ea3f1019de0
azapokou-art <azapokou@gmail.com>
2026-01-18 00:05:33 -0300

    Adiciona PasswordResetTokenRepository no dominio

---
+++ src/domain/repositories/PasswordResetTokenRepository.js
@@ -0,0 +1,15 @@
+class PasswordResetTokenRepository {
+    async create(tokenData) {
+        throw new Error('Method not implemented');
+    }
+    async findByToken(token) {
+        throw new Error('Method not implemented');
+    }
+    async invalidate(token) {
+        throw new Error('Method not implemented');
+    }
+    async deleteExpiredTokens() {
+        throw new Error('Method not implemented');
+    }
+}
+module.exports = PasswordResetTokenRepository;
