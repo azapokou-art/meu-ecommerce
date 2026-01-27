1caa1b33c36f741726f6740cf58d92ae2b8522cb
azapokou-art <azapokou@gmail.com>
2026-01-26 22:00:57 -0300

    Adiciona CategoryRepository na camada de dominio

---
+++ src/domain/repositories/CategoryRepository.js
@@ -0,0 +1,15 @@
+class CategoryRepository {
+    async create(category) {
+        throw new Error('Method not implemented');
+    }
+    async findAll() {
+        throw new Error('Method not implemented');
+    }
+    async findById(id) {
+        throw new Error('Method not implemented');
+    }
+    async findProductsByCategoryId(categoryId) {
+        throw new Error('Method not implemented');
+    }
+}
+module.exports = CategoryRepository;
