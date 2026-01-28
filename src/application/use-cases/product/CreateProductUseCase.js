0eefae89f62df479ad113026da147608245fedf8
azapokou-art <azapokou@gmail.com>
2026-01-28 17:31:00 -0300

    Adiciona CreateProductUseCase na camada aplicação

---
+++ src/application/use-cases/product/CreateProductUseCase.js
@@ -0,0 +1,40 @@
+class CreateProductUseCase {
+    constructor(productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    async execute({
+        name,
+        description,
+        price,
+        stock_quantity,
+        category_id,
+        image_url,
+        featured
+    }) {
+        if (!name) {
+            throw new Error('Product name is required');
+        }
+
+        if (price === undefined || price === null) {
+            throw new Error('Product price is required');
+        }
+
+        const product = {
+            name,
+            description: description || '',
+            price: Number(price),
+            stock_quantity: stock_quantity ? Number(stock_quantity) : 0,
+            category_id: category_id || null,
+            image_url: image_url || '',
+            featured: Boolean(featured)
+        };
+
+        const productId = await this.productRepository.create(product);
+
+        return productId;
+    }
+}
+
+module.exports = CreateProductUseCase;
+
