3d3d0fcf3aa73eea5c2bf2ef183c41f530b121bc
azapokou-art <azapokou@gmail.com>
2026-01-28 16:36:25 -0300

    Adiciona ProductRepository na camada de dominio

---
+++ src/domain/repositories/ProductRepository.js
@@ -0,0 +1,18 @@
+class ProductRepository {
+    async create(product) {
+        throw new Error('Method not implemented.');
+    }
+    async finsAll() {
+        throw new Error('Method not implemented.');
+    }
+    async findById(id) {
+        throw new Error('Method not implemented.');
+    }
+    async search(filters) {
+        throw new Error('Method not implemented.');
+    }
+    async count(filters) {
+        throw new Error('Method not implemented.');
+    }
+}
+module.exports = ProductRepository;
