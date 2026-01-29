cfdca941dda6adca291c85dfc85dea2232231203
azapokou-art <azapokou@gmail.com>
2026-01-28 21:51:56 -0300

    Atualiza o atualiza rotas no productRoutes

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const productController = require('../interface/handler/productHandler');
+const productImageHandler = require('../interface/handler/productImageHandler');
 const authMiddleware = require('../middlewares/authMiddleware');
 const upload = require('../infrastructure/upload');
 
+
 const router = express.Router();
 
-// Rotas públicas
+
 router.get('/', productController.getAll);
 router.get('/search', productController.search);
 router.get('/:id', productController.getById);
 
-// Rotas protegidas (apenas admin)
+
 router.post('/', authMiddleware, productController.create);
-router.post('/upload', authMiddleware, upload.single('image'), productController.uploadImage);
+router.post('/upload', authMiddleware, upload.single('image'), productImageHandler.upload);
 
 module.exports = router;
