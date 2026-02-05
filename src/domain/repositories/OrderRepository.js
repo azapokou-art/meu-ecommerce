38e148e9ede57212a5d492b53adca0ef75d1f67b
azapokou-art <azapokou@gmail.com>
2026-02-04 21:59:55 -0300

    Adiciona OrderRepository na camad de dominio

---
+++ src/domain/repositories/OrderRepository.js
@@ -0,0 +1,24 @@
+class OrderRepository {
+
+    create(orderData) {
+        throw new Error('Method not implemented');
+    }
+
+    addItem(orderId, itemData) {
+        throw new Error('Method not implemented');
+    }
+
+    updateStatus(orderId, status) {
+        throw new Error('Method not implemented');
+    }
+
+    findByUserId(userId) {
+        throw new Error('Method not implemented');
+    }
+
+    findById(orderId) {
+        throw new Error('Method not implemented');
+    }
+}
+
+module.exports = OrderRepository;
