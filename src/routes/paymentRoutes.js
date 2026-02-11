ed6b13d8684236c118f4dd068a77cbf8f0c5e815
azapokou-art <azapokou@gmail.com>
2026-02-11 19:22:19 -0300

    Atualiza o paymentRoutes

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,15 +1,12 @@
 const express = require('express');
-const paymentController = require('../interface/handler/paymentHandler');
+const paymentHandler = require('../interface/handler/paymentHandler');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
-
-router.post('/webhook', express.raw({type: 'application/json'}), paymentController.handleWebhook);
-
-
-router.post('/create-session', authMiddleware, express.json(), paymentController.createPaymentSession);
-router.get('/success', paymentController.paymentSuccess);
-router.get('/cancel', paymentController.paymentCancel);
+router.post('/webhook', express.raw({ type: 'application/json' }), paymentHandler.handleWebhook);
+router.post('/create-session', authMiddleware, paymentHandler.createPaymentSession);
+router.get('/success', paymentHandler.paymentSuccess);
+router.get('/cancel', paymentHandler.paymentCancel);
 
 module.exports = router;
